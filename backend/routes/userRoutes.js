f75db2a7c228aec5b1075d20631b263204341b8f
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2023-01-08 23:31:50 +0530

    initial changes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+
+const router = express.Router();
+
+router.route("/").post(registerUser).get(allUsers);
+router.route("/login").post(authUser);
+// router.post("/login", authUser);
+
+module.exports = router;
---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,52 @@
+import {
+  Box,
+  Container,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tab,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+
+const HomePage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Chit-Chat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={3} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
